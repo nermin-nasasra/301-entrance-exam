5f6ac3616a1ec8925d01d6fbb9caf032764918c9
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-03-16 12:50:01 +0200

    q1 done

---
+++ q1.js
@@ -0,0 +1,13 @@
+'use strict'
+var array =[1,2,3,4,5];
+var a=4;
+
+function greaterThan(){
+
+    for(var i=0;i<array.length;i++){
+        if(a>array[i]){
+            console.log(true);
+        }
+    }
+}
+
