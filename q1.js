2ecbb2e4ad99aca0a1585d75212f71d5fca7a9cd
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-03-16 13:12:39 +0200

    q2 done

--- q1.js
+++ q1.js
@@ -1,13 +1,16 @@
 'use strict'
 var array =[1,2,3,4,5];
 var a=4;
-
+var count;
 function greaterThan(){
-
+count=0;
     for(var i=0;i<array.length;i++){
         if(a>array[i]){
-            console.log(true);
+count=count+1;
+            
         }
     }
-}
+console.log(count);
 
+}
+greaterThan();
---
+++ q2.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var str='*';
+var count;
+function star(){
+count='';
+for(var i=0;i<5;i++){
+  count=count+str;
+  console.log(count);
+}
+}
+
+
+star();
